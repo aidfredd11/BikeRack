07f8fa013fc607326fd590c6e1364bdc50036ab8
Aidan <aidfrederick11@gmail.com>
2024-09-04 14:01:50 -0400

    Implemented LocationsListView toggle

--- BikeRackApp/BikeRackApp/Views/LocationsView.swift
+++ BikeRackApp/BikeRackApp/Views/LocationsView.swift
@@ -16,7 +16,45 @@ struct LocationsView: View {
         ZStack {
             Map(coordinateRegion: $vm.mapRegion)
                 .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                header
+                    .padding()
+                
+                Spacer()
+                
+            }
         }
+    }
+}
+
+extension LocationsView {
+    private var header: some View {
+        VStack {
+            Button(action: vm.toggleLocationsList){
+                Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundColor(.primary)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                
+                    .overlay(alignment: .leading) {
+                        Image(systemName: "arrow.down")
+                            .font(.headline)
+                            .foregroundColor(.primary)
+                            .padding()
+                            .rotationEffect(Angle(degrees: vm.showLocationsList ? -180 : 0))
+                    }
+            }
+            
+            if vm.showLocationsList {
+                LocationsListView()
+            }
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.3), radius: 20, x: 0, y: 15)
     }
 }
 
