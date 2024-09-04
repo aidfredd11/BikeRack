251b78cc3cf89a75f2390ac8b991dbc86ea1ce1b
Aidan <aidfrederick11@gmail.com>
2024-09-04 11:23:13 -0400

    Created new Swift app

---
+++ BikeRackApp/BikeRackApp/BikeRackAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeRackAppApp.swift
+//  BikeRackApp
+//
+//  Created by Aidan Frederick on 2024-09-04.
+//
+
+import SwiftUI
+
+@main
+struct BikeRackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BikeRackApp/BikeRackApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BikeRackApp
+//
+//  Created by Aidan Frederick on 2024-09-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
